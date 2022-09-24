362f43c68810aa48fa7f04d62a223a67e48b08ab
c-bertran <clement.bertrand22@gmail.com>
2022-09-24 15:20:53 +0200

    Add MCformat for string

--- src/api/formatString.ts
+++ src/api/formatString.ts
@@ -1,10 +1,36 @@
-export default function formatString(str: string, args: Record<string, unknown> | undefined): string {
-	if (!str || str.length <= 0)
+/**
+ * Parse and replace variable(s) inside string
+ * @param {string} str String to parse
+ * @param {object} args arguments to replace inside {...}
+ * @returns parsed string
+ */
+export default function formatString(str: string, args: Record<string, string> = {}): string {
+	let ret = '';
+	let occurenceKey = '';
+	let occurences = 0;
+	
+	if (!str || Object.entries(args).length === 0)
 		return str;
-	for (const char of str) {
-		console.log(char);
-		console.log('-');
+	for (let x = 0; x < str.length; x++) {
+		if (str[x] === '{')
+			++occurences;
+		else if (str[x] === '}') {
+			--occurences;
+			if (occurences === 0) {
+				const key = occurenceKey.slice(1);
+				occurenceKey = '';
+				if (Object.prototype.hasOwnProperty.call(args, key))
+					ret += args[key];
+				else
+					ret += key;
+				continue;
+			}
+		}
+		if (occurences === 0)
+			ret += str[x];
+		else
+			occurenceKey += str[x];
 	}
-	console.log(args);
-	return str;
+	console.log(ret);
+	return ret;
 };
--- src/store/global.ts
+++ src/store/global.ts
@@ -1,10 +1,13 @@
 import { defineStore } from 'pinia';
-import { ref, reactive } from 'vue';
+import { computed, ref, reactive } from 'vue';
+
+import formatString from 'api/formatString';
 
 window.localStorage.setItem('darkMode', `__q_bool|${Number(window.env.darkMode())}`);
 window.localStorage.setItem('lang', `__q_strn|${window.env.lang()}`);
 
 export const globalStore = defineStore('global', () => {
+	//#region Global
 	const directory = reactive({
 		app: window.env.directory.app,
 		appData: window.env.directory.appData,
@@ -19,21 +22,35 @@ export const globalStore = defineStore('global', () => {
 	});
 	const darkMode = ref<boolean>(window.env.darkMode());
 	const lang = ref<string>(window.env.lang());
-
+	
 	function setLang(val: string) {
 		lang.value = val;
 	};
 	function setDarkMode(val: boolean) {
 		darkMode.value = val;
 	}
+	//#endregion Global
+
+	//#region Map
+	const map = ref<string>('');
+
+	const getDatapackName = computed((args: Record<string, string> = {}) => {
+		if (Object.entries(args).length === 0)
+			return formatString(pack.data, { name: map.value });
+		else
+			return formatString(pack.data, args);
+	});
+	//#endregion
 
 	return {
 		directory,
 		pack,
 		darkMode,
 		lang,
-
 		setLang,
-		setDarkMode
+		setDarkMode,
+
+		map,
+		getDatapackName,
 	};
 });
